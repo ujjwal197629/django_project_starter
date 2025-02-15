56fdb12f3ea03046e54a108128463cce4f3cee09
ujjwal197629 <ujwal.dumre@gmail.com>
2025-02-15 22:37:16 +0545

    chore:Update api/users/models.py and core/settings/base.py to reflect changes in project structure

--- api/users/models.py
+++ api/users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+import uuid
 
+from api.users.managers import CustomUserManager
 # Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(_('email address'), unique=True)
+    username = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+    
+    def __str__(self):
+        return self.email
--- core/settings/base.py
+++ core/settings/base.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "django_project_starter.urls"
+ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "django_project_starter.wsgi.application"
+WSGI_APPLICATION = "core.wsgi.application"
 
 
 # Database
